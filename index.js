cfc1d2f79823bb4fdd1e6ac44ff5e62c2b2f65ad
Dark Rider <mormehtar@gmail.com>
2015-08-11 16:39:25 +0300

    DBEnv second approach the untested

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+var Table = require("./table");
+
 function DBEnv (dao) {
     this.dao = dao;
+    this.tables = {};
+    this.finalized = false;
 }
 
 module.exports = DBEnv;
+
+DBEnv.prototype.addTable = function (name) {
+    if (this.finalized) {
+        return null;
+    }
+    var table = new Table(name, this.dao[name]);
+    this.tables[name] = {
+        table: table,
+        priority: -1,
+        depends: {}
+    };
+    this.finalized = false;
+    return table;
+};
+
+DBEnv.prototype.finalize = function () {
+    if (this.finalized) {
+        return true;
+    }
+
+};
