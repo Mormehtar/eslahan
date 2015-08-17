20780d5c034f3725db72d364078969512c10cb9a
Dark Rider <mormehtar@gmail.com>
2015-08-17 16:53:10 +0300

    finalize func for env the untested + some tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Table = require("./table");
+var DBEnvError = require("./errors");
 
 function DBEnv (dao) {
     this.dao = dao;
@@ -10,21 +11,98 @@ module.exports = DBEnv;
 
 DBEnv.prototype.addTable = function (name) {
     if (this.finalized) {
-        return null;
+        throw new DBEnvError("Can`t add table to finalized table");
+    }
+    if (this.tables[name]) {
+        throw new DBEnvError("Can`t add table " + name + " again");
     }
     var table = new Table(name, this.dao[name]);
     this.tables[name] = {
         table: table,
         priority: -1,
-        depends: {}
+        dependencies: -1,
+        depends: []
     };
-    this.finalized = false;
+    this.tableOrder = [];
     return table;
 };
 
+DBEnv.prototype.getTable = function (tableName) {
+    var table = this.tables[tableName];
+    if (!table) { throw new DBEnvError("Cant`find table " + tableName); }
+    return table.table;
+};
+
+function checkTables (table, unfinished, finished) {
+    var newFinished = [];
+    for (var i = unfinished.length; --i;) {
+        var checkingTable = unfinished[i];
+        if (checkingTable.depends[table.table.name]) {
+            checkingTable.priority = Math.max(checkingTable.priority, table.priority + 1);
+            delete checkingTable.depends[table.table.name];
+            --checkingTable.dependencies;
+            if (checkingTable.dependencies == 0) {
+                newFinished.push(checkingTable);
+                unfinished.splice(i, 1);
+            }
+        }
+    }
+    newFinished.forEach(function (newFinishedTable) {
+        checkTables(newFinishedTable, unfinished, finished);
+    });
+    finished = finished.concat(newFinished);
+}
+
 DBEnv.prototype.finalize = function () {
     if (this.finalized) {
-        return true;
+        throw new DBEnvError("Can`t finalize finalized environment!");
+    }
+    var self = this;
+    var finished = [];
+    var unfinished = [];
+    var tables = Object.keys(self.tables).map(function (tableName) {
+        var table = self.tables[tableName];
+        table.table.finalize();
+        var fields = table.table.fields;
+        table.dependencies = 0;
+        table.depends = Object.keys(fields).map(function (fieldName) {
+            var dependency = fields[fieldName].dependency;
+            return dependency && dependency.name;
+        }).filter(function (table) {
+            if (table) {
+                ++table.dependencies;
+                return true;
+            }
+        }).reduce(function (obj, table) {
+            obj[table] = true;
+            return obj;
+        }, {});
+        if (table.dependencies === 0) {
+            table.priority = 0;
+            finished.push(table);
+        } else {
+            unfinished.push(table);
+        }
+        return self.tables[tableName];
+    });
+
+    for (var i=unfinished.length; --i;) {
+        checkTables(finished[i], unfinished, finished);
     }
 
+    if (unfinished.length) {
+        throw DBEnvError("There are table with open dependencies: " + unfinished.map(function (table) {
+                return table.table.name;
+            }).join(", "));
+    }
+
+    tables.sort(function (t1, t2) {
+        return t2.priority - t2.priority;
+    });
+
+    this.tableOrder = tables.map(function (table) {
+        return table.table.name;
+    });
+
+    return this.finalized = true;
 };
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,9 @@
 var assert = require("chai").assert;
 
+var TableDao = require("./testHelpers").tableDao;
+var Table = require("../table");
+var DBEnvError = require("../errors");
+
 var DBEnv = require("..");
 
 describe("DBEnv object", function () {
@@ -15,5 +19,51 @@ describe("DBEnv object", function () {
     it("Should save DAO from construction", function () {
         var dao = {};
         assert.equal((new DBEnv(dao)).dao, dao);
+    });
+
+    describe("addTable method", function () {
+        it("Should return Table object and save it", function () {
+            var env = new DBEnv({"SomeTable": new TableDao()});
+            var table = env.addTable("SomeTable");
+            assert.instanceOf(table, Table);
+        });
+
+        it("Should fail if dao does not support table", function () {
+            var env = new DBEnv({});
+            assert.throw(function () {
+                env.addTable("SomeTable");
+            }, DBEnvError);
+        });
+
+        it("Should throw exception on adding table again", function () {
+            var env = new DBEnv({"SomeTable": new TableDao()});
+            env.addTable("SomeTable");
+            assert.throw(function () {
+                env.addTable("SomeTable");
+            }, DBEnvError);
+        });
+
+        it("Should throw error on adding table into finalized env", function () {
+            var env = new DBEnv({"SomeTable": new TableDao()});
+            env.finalize();
+            assert.throw(function () {
+                env.addTable("SomeTable");
+            }, DBEnvError);
+        });
+    });
+
+    describe("getTable method", function () {
+        it("Should return table by demand", function () {
+            var env = new DBEnv({"SomeTable": new TableDao()});
+            var table = env.addTable("SomeTable");
+            assert.equal(table, env.getTable("SomeTable"));
+        });
+
+        it("Should throw error on getting not existent table", function () {
+            var env = new DBEnv({"SomeTable": new TableDao()});
+            assert.throw(function () {
+                env.getTable("SomeTable")
+            }, DBEnvError);
+        });
     });
 });
