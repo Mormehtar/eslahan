a1d7ed0b091bc4bc4888a25860f86f1235f1a1e3
Dark Rider <mormehtar@gmail.com>
2015-08-18 21:15:07 +0300

    moved fields to baseGenerator. Fields are nullable now

--- fields/text.js
+++ fields/text.js
@@ -1,16 +1,18 @@
 var LATIN_SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 var LATIN_LENGTH = LATIN_SYMBOLS.length;
-module.exports = function (orptions) {
-    var _options = orptions || {};
-    var length = _options.hasOwnProperty("length") ? (_options.length || 0) : 5;
-    var dispertion = _options.hasOwnProperty("dispertion") ? (_options.dispertion || 0) : 3;
-    var minimal = Math.max(length - dispertion, 1);
-    var maximal = Math.max(length + dispertion, 0);
-    var probability = 0.5 / Math.max(dispertion, 1);
-    return  function (value) {
-        if (arguments.length > 0) {
-            return value;
-        }
+
+var baseGenerator = require("./utils/baseGenerator");
+
+var defaults = {
+    length: 5,
+    dispertion: 3
+};
+
+var specificGenerator = function (options) {
+    var minimal = Math.max(options.length - options.dispertion, 1);
+    var maximal = Math.max(options.length + options.dispertion, 0);
+    var probability = 0.5 / Math.max(options.dispertion, 1);
+    return  function () {
         var result = "";
         var length = 0;
         while (length < minimal || !(Math.random() < probability || length >= maximal)) {
@@ -19,4 +21,8 @@ module.exports = function (orptions) {
         }
         return result;
     }
+};
+
+module.exports = function (options) {
+    return baseGenerator({options: options, defaults: defaults, specificGenerator: specificGenerator});
 };
